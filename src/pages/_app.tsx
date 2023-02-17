9660881d1e963d8c6ca9dc7eafc36d9d57e056e2
João Dias <joaoeduardodias123@gmail.com>
2023-02-17 10:27:26 -0300

    feat: install styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import { GlobalStyles } from '@/styles/global'
+import { defaultTheme } from '@/styles/themes/defaultTheme'
 import type { AppProps } from 'next/app'
-
+import { ThemeProvider } from 'styled-components'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from 'styled-components'
+export const GlobalStyles = createGlobalStyle`
+
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+
+  ::-webkit-scrollbar {
+      width: 0.45rem;
+    }
+  ::-webkit-scrollbar-track {
+      background: ${({ theme }) => theme.colors['base-border']}
+    }
+  ::-webkit-scrollbar-thumb {
+      background: ${({ theme }) => theme.colors['brand-blue']};
+      border-radius: 999px;
+    }
+}
+:focus {
+  outline: 0;
+  box-shadow: 0 0 0 1px ${({ theme }) => theme.colors['brand-blue']};
+}
+
+ body, input, textarea, button {
+  font: 400 ${({ theme }) => theme.textSizes['text-text-m']} Nunito,sans-serif;
+  line-height: 160%;
+ }  
+
+ body {
+  background-color: ${(props) => props.theme.colors['base-background']};
+  color: ${({ theme }) => theme.colors['base-text']};
+  -webkit-font-smoothing: antialiased;
+}
+
+ul {
+  list-style: none;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+button {
+  cursor: pointer;
+}
+
+
+`
